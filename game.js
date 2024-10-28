7fed41d625793a438ef9144c41c9ddbe7d934cd2
Aditya Singh <singhaditya2962@gmail.com>
2024-10-28 19:31:26 +0530

    compatible with mobile game

--- game.js
+++ game.js
@@ -1,52 +1,58 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
 var userClickedPattern = [];
-
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
+$("#button-27").click(function () {
   if (!started) {
     $("#level-title").text("Level " + level);
+    $("#button-27").hide(); 
+    $("#restart-button").hide(); 
     nextSequence();
     started = true;
   }
 });
 
-$(".btn").click(function() {
+$("#restart-button").click(function () {
+  startOver(); 
+  $("#level-title").text("Level " + level);
+  $("#button-27").hide();
+  $("#restart-button").hide();
+  nextSequence();
+  started = true;
+});
 
+$(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      startOver();
+        nextSequence();
+      }, 1000);
     }
-}
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over! Press Restart to Try Again");
 
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#restart-button").show();
+    started = false;
+  }
+}
 
 function nextSequence() {
   userClickedPattern = [];
@@ -76,4 +82,6 @@ function startOver() {
   level = 0;
   gamePattern = [];
   started = false;
+  $("#button-27").show(); 
+  $("#restart-button").hide();
 }
